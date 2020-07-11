db38c375acde8215c11a487ea35a231cdde907de
aldocesarmg <aldocesarmg@gmail.com>
2020-07-11 17:48:31 -0500

    Creacion de Material List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+// whenever you want to make any component or a module portable in
+// another module, then you would always prepend the export to the class here.
+
+// siempre que se quiera exportar un componente o modulo para poder ser
+// importado en otra clase, se debe utilizar la palabra reservada "export"
 export class AppComponent {
   title = 'conFusion';
 }
