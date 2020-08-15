abcef9b66ca58439dc8cb55f73276b224d07bdc8
aldocesarmg <aldocesarmg@gmail.com>
2020-08-15 14:06:45 -0500

    RxJS part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,11 +23,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getFeaturedDish()
-      .then(dish => this.dish = dish);
+      .subscribe(dish => this.dish = dish);
     this.promotionService.getFeaturedPromotions()
-      .then(promotion => this.promotion = promotion);
+      .subscribe(promotion => this.promotion = promotion);
     this.leaderService.getFeaturedLeader()
-      .then(leaders => this.leader = leaders);
+      .subscribe(leaders => this.leader = leaders);
   }
 
 }
